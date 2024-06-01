2ce813c2d3d277f6bc2549f5382652f5078f5d14
ZakyZN <zakyzn1999@gmail.com>
2024-06-01 18:48:41 +0800

    new

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <nav-bar/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NavBar from './components/Nav-bar.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    NavBar
   }
 }
 </script>
@@ -19,8 +18,6 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Nav-bar.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <nav class="flex justify-between items-center p-4 bg-white shadow-md">
+            <a href="#"><img src="../assets/Logo-1.png" alt="Logo" ></a>
+            <ul class="hidden md:flex space-x-8">
+                <li><a href="#" class="hover:text-gray-600">Home</a></li>
+                <li><a href="#" class="hover:text-gray-600">About</a></li>
+                <li><a href="#" class="hover:text-gray-600">Product</a></li>
+                <li><a href="#" class="hover:text-gray-600">Contact</a></li>
+            </ul>
+            <button @click="toggleMenu" class="md:hidden flex items-center text-xl">
+                ☰
+            </button>
+        </nav>
+        <div v-if="isMenuActive" class="md:hidden bg-white shadow-md p-4 flex flex-col space-y-4">
+            <a href="#" class="hover:text-gray-600">Home</a>
+            <a href="#" class="hover:text-gray-600">About</a>
+            <a href="#" class="hover:text-gray-600">Product</a>
+            <a href="#" class="hover:text-gray-600">Contact</a>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'NavBar',
+    data() {
+        return {
+            isMenuActive: false
+        };
+    },
+    methods: {
+        toggleMenu() {
+            this.isMenuActive = !this.isMenuActive;
+        }
+    }
+}
+</script>
